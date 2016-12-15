1830f5c73933295db45dee16d8801bdff71506fb
Craig Dearden <craigrdearden2@gmail.com>
2016-12-14 19:43:12 -0800

    (fix) missing startTime property in Ambit schema

--- server/ambitData/ambitSchema.js
+++ server/ambitData/ambitSchema.js
@@ -12,6 +12,7 @@ var ambitSchema = new Schema({
   },
   weekdays: [Boolean], //0 is Sunday, 6 is Saturday
   startDate: Date,
+  startTime: String,
   checkIns: [Date] // a history of successful check-ins
   //time (when during the day are you supposed to check in)
   //repeats (every week? every other week? is this necessary?)
