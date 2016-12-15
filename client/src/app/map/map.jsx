65466f929d685848b8c4132aafec9496cdfcb6a2
pengjoshua <joshuapeng7@gmail.com>
2016-12-14 19:18:59 -0800

    (feat/markers) Move time/mode text fields

--- client/src/app/map/map.jsx
+++ client/src/app/map/map.jsx
@@ -91,20 +91,23 @@ const floatingLabelFocusStyle = {
 };
 
 const zoomTextStyle = {
+  position: 'fixed',
+  top: '5%',
+  left: 'calc(1% + 300px)',
   color: Colors.lime600
 };
 
 const selectTimeStyle = {
-  position: "fixed",
+  position: 'fixed',
   top: '5%',
-  left: "calc(1% + 0px)",
+  left: 'calc(1% + 20px)',
   width: 130
 };
 
 const selectModeStyle = {
-  position: "fixed",
+  position: 'fixed',
   top: '5%',
-  left: "calc(1% + 130px)",
+  left: 'calc(1% + 160px)',
   width: 130
 };
 
