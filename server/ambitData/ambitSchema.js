aca0473e5d909684853ea0b3007bc050d4ce9746
William Powelson <willpowelson@gmail.com>
2016-12-16 16:43:06 -0800

    updated ambit schema to include stats, pre-hook as well

--- server/ambitData/ambitSchema.js
+++ server/ambitData/ambitSchema.js
@@ -10,13 +10,29 @@ var ambitSchema = new Schema({
     latitude: Number,
     longitude: Number
   },
+  createdAt: Date,
+  stats: {
+    lastUpdate: Date,
+    missed:{type: [Number], default: [0,0,0,0,0,0,0]},
+    made:{type: [Number], default: [0,0,0,0,0,0,0]}
+  },
   weekdays: [Boolean], //0 is Sunday, 6 is Saturday
   startDate: Date,
   startTime: String,
   checkIns: [Date] // a history of successful check-ins
-
   //time (when during the day are you supposed to check in)
   //repeats (every week? every other week? is this necessary?)
+});
+
+ambitSchema.pre('save', function(next){
+  now = new Date();
+  if (!this.created_at) {
+    this.createdAt = now;
+  }
+  if (!this.stats.lastUpdate) {
+    this.stats.lastUpdate = now;
+  }
+  next();
 });
 
 var Ambit = mongoose.model('Ambit', ambitSchema);
